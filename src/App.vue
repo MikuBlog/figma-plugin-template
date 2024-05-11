3184427cb93800b9fda6f7eb951f1f387bbf4ad8
旋仔 <zixuan.wen@shopcider.com>
2024-05-11 19:25:54 +0800

    perf

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
+<!--
+ * @Author: 旋仔 zixuan.wen@shopcider.com
+ * @Date: 2024-05-11 18:08:49
+ * @LastEditors: 旋仔 zixuan.wen@shopcider.com
+ * @LastEditTime: 2024-05-11 19:24:46
+ * @FilePath: /figma-plugin-vue3-template/src/App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
-  <div />
+  <div>
+    <HelloWorld />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
+  components: { HelloWorld },
   setup (props, context) {
     return {
 
