e6be4866a0cdde7c223870096ac35635b1b0cea6
旋仔 <zixuan.wen@shopcider.com>
2024-05-13 14:11:31 +0800

    fix: 修复别名路径

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: 旋仔 zixuan.wen@shopcider.com
  * @Date: 2024-05-11 18:08:49
  * @LastEditors: 旋仔 zixuan.wen@shopcider.com
- * @LastEditTime: 2024-05-13 14:10:15
+ * @LastEditTime: 2024-05-13 14:11:20
  * @FilePath: /figma-plugin-vue3-template/vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -34,8 +34,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, '../src'),
-      '~': resolve(__dirname, '../'),
+      '@': resolve(__dirname, './src'),
+      '~': resolve(__dirname, './'),
     },
   },
   build: {
