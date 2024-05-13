89a86d4e1ea1fd33411a0bf687d510dbcf802d74
旋仔 <zixuan.wen@shopcider.com>
2024-05-13 14:57:51 +0800

    feat: 更新配置

--- figma/code.ts
+++ figma/code.ts
@@ -1,4 +1,11 @@
-/// <reference types="@figma/plugin-typings" />
+/*
+ * @Author: 旋仔 zixuan.wen@shopcider.com
+ * @Date: 2024-05-11 18:08:49
+ * @LastEditors: 旋仔 zixuan.wen@shopcider.com
+ * @LastEditTime: 2024-05-13 14:57:25
+ * @FilePath: /figma-plugin-vue3-template/figma/code.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // This plugin will open a window to prompt the user to enter a number, and
 // it will then create that many rectangles on the screen.
 
