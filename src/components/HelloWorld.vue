d0be25a5387fc295bd84b73f6d8dda11f2520d81
旋仔 <zixuan.wen@shopcider.com>
2024-05-13 18:55:49 +0800

    feat: 支持pinia

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,29 @@
+<!--
+ * @Author: 旋仔 zixuan.wen@shopcider.com
+ * @Date: 2024-05-11 18:08:49
+ * @LastEditors: 旋仔 zixuan.wen@shopcider.com
+ * @LastEditTime: 2024-05-13 18:54:59
+ * @FilePath: /figma-plugin-vue3-template/src/components/HelloWorld.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <div class="color-red fw-bold">
-    Hello World
+    Hello World!
+    <button @click="() => store.decrease()">
+      count: {{ store.count }}
+    </button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { useStore } from '@/store'
 
 export default defineComponent({
   setup (props, context) {
+    const store = useStore()
     return {
-
+      store,
     }
   },
 })
