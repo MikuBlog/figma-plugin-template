fd7f3f40ca95ee5739ca7b80b1a7473ad41bd6ab
旋仔 <zixuan.wen@shopcider.com>
2024-05-13 18:39:26 +0800

    feat: 支持自动导入全局组件

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: 旋仔 zixuan.wen@shopcider.com
  * @Date: 2024-05-11 18:08:49
  * @LastEditors: 旋仔 zixuan.wen@shopcider.com
- * @LastEditTime: 2024-05-13 14:11:20
+ * @LastEditTime: 2024-05-13 18:38:51
  * @FilePath: /figma-plugin-vue3-template/vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,11 +12,18 @@ import { viteSingleFile } from 'vite-plugin-singlefile'
 import UnoCSS from 'unocss/vite'
 import { resolve } from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     UnoCSS(),
+    Components({
+      dirs: [resolve(__dirname, './src/components')],
+      extensions: ['vue'],
+      deep: true, // 搜索子目录
+      dts: './src/components.d.ts',
+    }),
     AutoImport({
       imports: [
         // presets
