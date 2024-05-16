b6cb182f2600dbb400c811c700379825203a5c85
旋仔 <zixuan.wen@shopcider.com>
2024-05-16 11:05:04 +0800

    feat: 支持element-plus，按需引入

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  * @Author: 旋仔 zixuan.wen@shopcider.com
  * @Date: 2024-05-11 18:08:49
  * @LastEditors: 旋仔 zixuan.wen@shopcider.com
- * @LastEditTime: 2024-05-13 18:59:06
+ * @LastEditTime: 2024-05-16 11:04:42
  * @FilePath: /figma-plugin-vue3-template/README.md
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -10,6 +10,7 @@
 
 - [x] vue3
 - [x] pinia
+- [x] element-plus
 - [x] unocss
 - [x] vite
 - [x] typescript
@@ -17,6 +18,7 @@
 - [x] change-log
 - [x] unplugin-auto-import
 - [x] unplugin-vue-components
+- [x] unplugin-element-plus
 
 ## dev (watch)
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: 旋仔 zixuan.wen@shopcider.com
  * @Date: 2024-05-11 18:08:49
  * @LastEditors: 旋仔 zixuan.wen@shopcider.com
- * @LastEditTime: 2024-05-13 18:38:51
+ * @LastEditTime: 2024-05-16 11:04:14
  * @FilePath: /figma-plugin-vue3-template/vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,6 +13,7 @@ import UnoCSS from 'unocss/vite'
 import { resolve } from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -33,10 +34,16 @@ export default defineConfig({
     }),
     vue(),
     viteSingleFile(),
+    ElementPlus({
+      useSource: true,
+    }),
   ],
   css: {
     preprocessorOptions: {
       less: {},
+      scss: {
+        additionalData: '@use "@/styles/element-plus.scss" as *;',
+      },
     },
   },
   resolve: {
@@ -46,10 +53,8 @@ export default defineConfig({
     },
   },
   build: {
-    // https://vitejs.cn/config/#build-csscodesplit
     cssCodeSplit: false,
-    // https://vitejs.cn/config/#build-assetsinlinelimit
-    assetsInlineLimit: 100000000,
+    assetsInlineLimit: 1024 * 1024, // 1MB
     rollupOptions: {
       input: {
         index: 'index.html',
