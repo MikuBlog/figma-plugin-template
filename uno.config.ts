01a2512b7ff127c2a834271568eb9a8deff6e910
旋仔 <zixuan.wen@shopcider.com>
2024-05-21 10:59:23 +0800

    feat: 增加快捷指令

--- uno.config.ts
+++ uno.config.ts
@@ -2,8 +2,8 @@
  * @Description:
  * @Author: xuanzai
  * @Date: 2024-04-19 14:08:49
- * @LastEditors: xuanzai
- * @LastEditTime: 2024-04-19 14:31:33
+ * @LastEditors: 旋仔 zixuan.wen@shopcider.com
+ * @LastEditTime: 2024-05-21 10:56:43
  */
 // uno.config.ts
 import { defineConfig } from 'unocss'
@@ -12,5 +12,8 @@ import presetUno from '@unocss/preset-uno'
 export default defineConfig({
   presets: [
     presetUno(),
+  ],
+  shortcuts: [
+    { 'absolute-center': 'absolute top-0 right-0 left-0 bottom-0 m-auto' },
   ],
 })
